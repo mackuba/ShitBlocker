aafc8d02c7bcde90eba3e37e489c5a161724a6cb
Kuba Suder <jakub.suder@gmail.com>
2017-11-23 00:33:34 +0100

    added content blocker

---
+++ ContentBlocker/ContentBlockerRequestHandler.swift
@@ -0,0 +1,22 @@
+//
+//  ContentBlockerRequestHandler.swift
+//  ContentBlocker
+//
+//  Created by Kuba Suder on 23.11.2017.
+//  Copyright © 2017 Kuba Suder. All rights reserved.
+//
+
+import Foundation
+
+class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+        
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+        
+        context.completeRequest(returningItems: [item], completionHandler: nil)
+    }
+    
+}
