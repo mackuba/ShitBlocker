0c4300fdbbec4605579c0f83660db762ef3cf343
Kuba Suder <jakub.suder@gmail.com>
2017-11-23 00:06:07 +0100

    created project

---
+++ ShitBlocker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ShitBlocker
+//
+//  Created by Kuba Suder on 23.11.2017.
+//  Copyright © 2017 Kuba Suder. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
